97601b7fbcf9d46c841b2da20ca1ee7b578a529b
Mawote de Melo <mawote@mawote.com>
2020-02-01 23:43:10 +0100

    Criada a primeira classe do projeto Posicao

--- src/com/mawote/aplication/JogoDeXadrezProgram.java
+++ src/com/mawote/aplication/JogoDeXadrezProgram.java
@@ -1,10 +1,13 @@
 package com.mawote.aplication;
 
+import com.mawote.tabuleiro.Posicao;
+
 public class JogoDeXadrezProgram {
 
 	public static void main(String[] args) {
 
-		
+		Posicao posicao = new Posicao(3, 5);
+		System.out.println(posicao);
 
 	}
 
